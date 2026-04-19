68afa82fffba9eb1cf1831916839684deb874a94
zu213 <hcazenotspu@gmail.com>
2026-04-19 22:30:42 +0100

    Switched to 2 space indent

--- S RankTests/S_RankTests.swift
+++ S RankTests/S_RankTests.swift
@@ -9,9 +9,9 @@ import Testing
 @testable import S_Rank
 
 struct S_RankTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    }
-
+  
+  @Test func example() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+  }
+  
 }
